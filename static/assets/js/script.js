7a9abce26a8d172f9836e87476896838a6fff2a4
Aarsh Chaube <65212523+Aarsh2001@users.noreply.github.com>
2021-07-11 11:59:46 +0530

    Static

---
+++ static/assets/js/script.js
@@ -0,0 +1,10 @@
+// By @AbubakerSaeed96 (twitter)
+// ===================
+
+// Images are from Unsplash.
+
+// Looks Good In Firefox
+// Font not displaying correctly in Chrome and Opera
+// Looks Good In Edge Dev (Almost)
+
+// Thank You For Viewing.
---
+++ static/assets/js/script_loader.js
@@ -0,0 +1,5 @@
+$(window).on('load',function(){
+	setTimeout(function(){
+	$('.page-loader').fadeOut('slow');
+	},54000);
+});
